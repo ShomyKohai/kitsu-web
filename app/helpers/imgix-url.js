46bc5752343ff676326507dfb6b628ec89e92e9e
Emma Lejeck <nuck@kitsu.io>
2024-08-15 05:56:27 +0000

    Make sure we ALWAYS generate a valid imgix url

--- app/helpers/imgix-url.js
+++ app/helpers/imgix-url.js
@@ -1,14 +1,17 @@
 import { helper } from 'ember-helper';
 
 export function imgixUrl([src, params = {}]) {
-  const url = new URL(
-    src
-      .replace('https://media.kitsu.app/', 'https://kitsu.imgix.net/')
-      .replace(
-        'https://media-staging.kitsu.app/',
-        'https://kitsu-staging.imgix.net/'
-      )
-  );
+  const url = new URL(src);
+  switch (url.hostname) {
+    case 'media.kitsu.io':
+    case 'media.kitsu.app':
+      url.hostname = 'kitsu.imgix.net';
+      break;
+    case 'media-staging.kitsu.io':
+    case 'media-staging.kitsu.app':
+      url.hostname = 'kitsu-staging.imgix.net'
+  }
+
   const existingParams = Object.fromEntries(new URLSearchParams(url.search));
   const options = new URLSearchParams({
     ...existingParams,
